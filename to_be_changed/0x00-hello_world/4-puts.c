f10f03eaa8be47837a5e3d78ea26bb9a2dd0a310
Adebukun <adedejizacchaeus@gmail.com>
2022-07-02 01:00:25 -0700

    All

---
+++ to_be_changed/0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!bin/bash
+#include <stdo.h>
+#include <string.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"):
+		return (0):
+}
