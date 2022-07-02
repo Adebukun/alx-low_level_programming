df74636ebd2c271f753e82755de343a9516872dc
Adebukun <adedejizacchaeus@gmail.com>
2022-07-02 03:18:38 -0700

    All

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /*
- * main - prints exactly "and that peice of art is usedful"
+ * main - prints exactly "and that piece of art is useful"
  * -Dora Korpar, 2015-10-19",
  *  followed by a new line, to the standard error
  *  Return: Always 0 (Success)
@@ -12,5 +12,5 @@ int main(void)
 
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-	return (1)
+	return (1);
 }
