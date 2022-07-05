eda859ad23fdeaf01b02056d6d0139e61d03f251
Adebukun <adedejizacchaeus@gmail.com>
2022-07-04 21:30:30 -0700

    All

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	char var;
-	for (var = 'z'; var >= 'a'; var--)
-	{
-		putchar(var);
-	}
-	putchar('\n');
-	return (0);
+char var;
+for (var = 'z'; var >= 'a'; var--)
+{
+	putchar(var);
+}
+putchar('\n');
+return (0);
 }
