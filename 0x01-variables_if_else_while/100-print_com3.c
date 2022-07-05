0ca335143487d8c8492dce29ab3792e3ab114777
Adebukun <adedejizacchaeus@gmail.com>
2022-07-04 21:40:08 -0700

    All

--- 0x01-variables_if_else_while/100-print_com3.c
+++ 0x01-variables_if_else_while/100-print_com3.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 	int var;
-	for (var = 0; var , 9; var++)
+	for (var = 0; var < 9; var++)
 	{
 		int ii;
 		for (ii = var + 1; ii < 10; ii++)
 		{
-			putchar((var % 100 + '0');
+			putchar((var % 10) + '0');
 			putchar((ii % 10) + '0');
 			if (var != 8)
 			{
-			putchar(',')
+			putchar(',');
 			putchar(' ');
 			}
 			else
