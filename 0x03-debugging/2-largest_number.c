e1ea9ff50999acf5bc509b13017e9ab3186f271f
Adebukun <adedejizacchaeus@gmail.com>
2022-07-07 21:34:41 -0700

    All

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,6 +1,11 @@
 #include "main.h"
+#ifndef MAIN_h
+#define MAIN_H
+
+#include<stdio.h>
+
 /**
- * largesst_number - returns the largest of 3 numbers
+ * largest_number - returns the largest of 3 numbers
  * @var1: first integer
  * @var2: seconf integer
  * @var3: third integer
@@ -8,14 +13,14 @@
  */
 int largest_number(int var1, int var2, int var3)
 {
-	int largest;
-	if (var1 > var2 && var1 > var3)
-		largest = var1;
-	else if (var2 > var1 && var2 > var3)
-		largest = var2;
-	else if (var3 > var2)
-		largest = var3;
-	else
-		largest = var2;
-	return (largest);
+int largest;
+if (var1 > var2 && var1 > var3)
+largest = var1;
+else if (var2 > var1 && var2 > var3)
+largest = var2;
+else if (var3 > var2)
+largest = var3;
+else
+largest = var2;
+return (largest);
 }
